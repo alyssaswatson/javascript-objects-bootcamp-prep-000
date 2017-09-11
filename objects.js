0c5bcac3a59634bcdd78b3be3682b94c5b535c78
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-11 19:29:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   artistName: "Tyler",
   songTitle: "Song Title"
 };
+
+function updatePlaylist (playlist, artistName songTitle) {
+  object.assign ({}, playlist, { artistName: ['artistName']})
+  object.assign ({}, playlist, { artistName: ['songTitle']})
+}
